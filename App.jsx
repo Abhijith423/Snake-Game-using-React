74f06c233c569f7421db18e9db7bd7f73acfe1f1
Abhijith <abhijithemes@gmail.com>
2025-09-01 14:00:30 +0530

    App.jsx

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import SnakeGame from './components/snake.jsx'
+
+function App() {
+
+  return (
+    <>
+    <div className='App'>
+      <SnakeGame />
+      </div>
+    </>
+  )
+}
+
+export default App
